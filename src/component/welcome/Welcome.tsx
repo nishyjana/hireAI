f08b2587a26c691143ef4518bc84cba154e8ff7e
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-04-10 14:07:18 +0530

    FEAT: basic route with layout implementation

--- src/component/welcome/Welcome.tsx
+++ src/component/welcome/Welcome.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { ABOUT } from "../../constants/PathConstants";
 import InputSearch from "../../ui/InputSearch";
 
 export default function Welcome() {
+  const history = useHistory()
   return (
     <div className="flex flex-col w-full items-center py-36 ">
       <h2 className=" font-semibold text-2xl font-sans flex mx-auto mb-14 mt-32">
         Hire AI
       </h2>
-      <InputSearch onclick={() => alert("f")} />
+      <InputSearch onclick={() => history.push(ABOUT)} />
     </div>
   );
 }
