cd7d62308650670af67b6011023c7b5d9f50a2e6
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-04-10 14:07:28 +0530

    FEAT: basic route with layout implementation

--- src/component/welcome/Welcome.tsx
+++ src/component/welcome/Welcome.tsx
@@ -4,7 +4,7 @@ import { ABOUT } from "../../constants/PathConstants";
 import InputSearch from "../../ui/InputSearch";
 
 export default function Welcome() {
-  const history = useHistory()
+  const history = useHistory();
   return (
     <div className="flex flex-col w-full items-center py-36 ">
       <h2 className=" font-semibold text-2xl font-sans flex mx-auto mb-14 mt-32">
