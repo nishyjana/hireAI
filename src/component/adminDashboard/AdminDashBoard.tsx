efa5e9ea9e3410106bdd79018649c5ddccbbf231
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-05-14 19:43:53 +0530

    added admin dashbaord basics

--- src/component/adminDashboard/AdminDashBoard.tsx
+++ src/component/adminDashboard/AdminDashBoard.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
 export default function AdminDashBoard() {
-    return <div className='p-20'>AdminDashBoard</div>;
+    return (
+        <div className="p-20 flex flex-col w-full">
+            <div className="p-20 flex  w-full">
+                <div className="m-auto flex">
+                    <div className="flex flex-col mx-2">
+                        <div className="mx-2 border-2 p-10 rounded-xl text-3xl font-bold border-b-8">
+                            03
+                        </div>
+                        <div className="m-2 text-center">Vacancies</div>
+                    </div>
+                    <div className="flex flex-col mx-2">
+                        <div className="mx-2 border-2 p-10 rounded-xl text-3xl font-bold border-b-8">
+                            36
+                        </div>
+                        <div className="m-2  text-center">Total Applications</div>
+                    </div>
+                    <div className="flex flex-col mx-2">
+                        <div className="mx-2 border-2 p-10 rounded-xl text-3xl font-bold border-b-8">
+                            23
+                        </div>
+                        <div className="m-2  text-center">Total Minutes</div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col m-auto p-20 w-3/4 border-2 border-b-8 rounded-2xl border-gray-300">
+                <div className="font-bold mb-10">DEADLINES</div>
+                <div>sales intern</div>
+            </div>
+        </div>
+    );
 }
--- src/routes/AdminRoutesWithLayout.tsx
+++ src/routes/AdminRoutesWithLayout.tsx
@@ -10,12 +10,13 @@ const ScreensWithNavAndSideBar = ({ children }) => (
                     <div className="fixed w-1/3 xl:w-1/5">
                         <Sidebar />
                     </div>
-                    <div className="flex w-2/3 xl:w-3/4">
-                        <div className="pl-64 xl:pl-80 flex w-full">
+                    
+                </div>
+                <div className="flex w-2/3 xl:w-3/4">
+                        <div className="pl-4 flex w-full">
                             <div className="pt-4 w-full">{children}</div>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     </>
