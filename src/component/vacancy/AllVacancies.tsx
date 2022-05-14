70ce1f97aea1ad2ba5891b27cdd316a65c958f2a
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-05-14 20:00:50 +0530

    added admin dashbaord basics

--- src/component/vacancy/AllVacancies.tsx
+++ src/component/vacancy/AllVacancies.tsx
@@ -3,35 +3,47 @@ import React from 'react';
 export default function AllVacancies() {
     return (
         <div className="p-20 flex flex-col w-full">
-            <div className='flex justify-end my-2 mb-10'>
+            <div className="flex justify-end my-2 mb-10">
                 <div className="w-1/5 bottom-2 bg-hireAI p-3 rounded-3xl text-white text-center justify-end">
                     Create vacancy +
                 </div>
             </div>
-            <div className="flex flex-col m-auto p-20 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300">
+            <div className="flex flex-col m-auto px-20 py-10 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300 my-2">
                 <div className="font-bold mb-1">Sales Intern - Batch 0049 </div>
                 <div className="text-sm mb-8">Deadline: 3rd March 2022</div>
                 <div>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium maximus
                     bibendum.
                 </div>
+                <div className="flex justify-end my-2 mb-3 mt-10">
+                    <div className=" text-center justify-end mx-2 text-gray-600">LOG PROGRESS</div>
+                    <div className=" text-center justify-end mx-2 text-gray-600">MORE INFO</div>
+                </div>
             </div>
 
-            <div className="flex flex-col m-auto p-20 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300 my-2">
+            <div className="flex flex-col m-auto px-20 py-10 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300 my-2">
                 <div className="font-bold mb-1">Sales Intern - Batch 0049 </div>
                 <div className="text-sm mb-8">Deadline: 3rd March 2022</div>
                 <div>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium maximus
                     bibendum.
                 </div>
+                <div className="flex justify-end my-2 mb-3 mt-10">
+                    <div className=" text-center justify-end mx-2 text-gray-600">LOG PROGRESS</div>
+                    <div className=" text-center justify-end mx-2 text-gray-600">MORE INFO</div>
+                </div>
             </div>
 
-            <div className="flex flex-col m-auto p-20 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300 my-2">
+            <div className="flex flex-col m-auto px-20 py-10 w-1/2 border-2 border-b-8 rounded-2xl border-gray-300 my-2">
                 <div className="font-bold mb-1">Sales Intern - Batch 0049 </div>
                 <div className="text-sm mb-8">Deadline: 3rd March 2022</div>
                 <div>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium maximus
                     bibendum.
+                </div>
+                <div className="flex justify-end my-2 mb-3 mt-10">
+                    <div className=" text-center justify-end mx-2 text-gray-600">LOG PROGRESS</div>
+                    <div className=" text-center justify-end mx-2 text-gray-600">MORE INFO</div>
                 </div>
             </div>
         </div>
