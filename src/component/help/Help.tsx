63a899c71e3256cd44acae076e10cb37ad0e2e37
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-05-19 19:38:07 +0530

    fixed the side bar

--- src/component/help/Help.tsx
+++ src/component/help/Help.tsx
@@ -10,7 +10,7 @@ export default function Help() {
                 <div className="flex flex-col">
                     <img src="images\Details.svg" className="w-full  h-48" alt="" />
                     <div className='text-center font-bold my-1'>ENTER YOUR DETAILS</div>
-                    <div className="mt-3 p-1 w-56 flex mx-auto">
+                    <div className="mt-3 p-1 w-56 flex mx-auto text-center">
                         Fill the application form displayed on the screen by providing truthful
                         information about you.
                     </div>
@@ -18,14 +18,14 @@ export default function Help() {
                 <div className="flex flex-col">
                     <img src="images\UploadCSV.svg" className="w-full h-48" alt="" />
                     <div className='text-center font-bold my-1'>UPLOAD CV</div>
-                    <div className="mt-3 p-1 w-56 flex mx-auto">
+                    <div className="mt-3 p-1 w-56 flex mx-auto text-center">
                         Upload the latest version of your curriculum vitae
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <img src="images\RecordInterview.svg" className="w-full  h-48" alt="" />
                     <div className='text-center font-bold my-1'>RECORD INTERVIEW</div>
-                    <div className="mt-3 p-1 w-56 flex mx-auto">
+                    <div className="mt-3 p-1 w-56 flex mx-auto text-center">
                         Start recording yourself answering the questions appearing on the screen and
                         end the recording once you are done.
                     </div>
