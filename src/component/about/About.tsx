ff6f01cb16423aff21dbb2bd177b5f368598b81d
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-05-19 19:36:23 +0530

    fixed the side bar

--- src/component/about/About.tsx
+++ src/component/about/About.tsx
@@ -5,21 +5,14 @@ export default function About() {
         <div className="flex flex-col w-full items-center py-36 ">
             <h2 className=" font-semibold text-2xl font-sans flex mx-auto mb-14 mt-32">ABOUT</h2>
             <div className="w-1/2 h-auto px-4 py-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est mi, molestie et
-                tellus sit amet, rutrum mollis felis. Etiam euismod maximus urna non fermentum.
-                Nullam vitae fermentum nibh.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vivamus est mi, molestie et tellus sit amet, rutrum mollis felis. Etiam euismod
-                maximus urna non fermentum. Nullam vitae fermentum nibh.Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Vivamus est mi, molestie et tellus sit amet, rutrum
-                mollis felis. Etiam euismod maximus urna non fermentum. Nullam vitae fermentum
-                nibh.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est mi,
-                molestie et tellus sit amet, rutrum mollis felis. Etiam euismod maximus urna non
-                fermentum. Nullam vitae fermentum nibh.Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Vivamus est mi, molestie et tellus sit amet, rutrum mollis felis.
-                Etiam euismod maximus urna non fermentum. Nullam vitae fermentum nibh.Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit. Vivamus est mi, molestie et tellus sit
-                amet, rutrum mollis felis. Etiam euismod maximus urna non fermentum. Nullam vitae
-                fermentum nibh.
+                Hire.AI is the first ever AI powered pre-screening tool on the domain of entry level
+                sales candidates. Hire. AI uses technologies like Facial analysis and sentiment
+                analysis to give a fully fledge analysis on the candidate’s communication potential.
+                Hire AI provides candidates simple Ui which is self-explanatory. The platform allows
+                the applicants to finish their applications with three easy simple steps which helps
+                the candidates to save time on their application. Further the platform provides you
+                with constant stats updates on your application reducing your work in following up
+                with different vacancies. Get yourself hired through our platform to assure today!
             </div>
         </div>
     );
--- src/component/help/Help.tsx
+++ src/component/help/Help.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 
 export default function Help() {
     return (
-        <div className="flex flex-col w-full items-center py-36 ">
+        <div className="flex flex-col w-full items-center ">
             <h2 className=" font-semibold text-2xl font-sans flex mx-auto mb-14  mt-32">
                 HOW IT WORKS
             </h2>
             <div className="w-full flex h-auto py-2 justify-evenly ml-10">
                 <div className="flex flex-col">
-                    <img src="images\Details.svg" className="w-full h-full" alt="" />
+                    <img src="images\Details.svg" className="w-full  h-48" alt="" />
+                    <div className='text-center font-bold my-1'>ENTER YOUR DETAILS</div>
                     <div className="mt-3 p-1 w-56 flex mx-auto">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est mi,
-                        molestie et tellus sit amet, rutrum mollis felis.
+                        Fill the application form displayed on the screen by providing truthful
+                        information about you.
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <img src="images\UploadCSV.svg" className="w-full h-full" alt="" />
+                    <img src="images\UploadCSV.svg" className="w-full h-48" alt="" />
+                    <div className='text-center font-bold my-1'>UPLOAD CV</div>
                     <div className="mt-3 p-1 w-56 flex mx-auto">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est mi,
-                        molestie et tellus sit amet, rutrum mollis felis.
+                        Upload the latest version of your curriculum vitae
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <img src="images\RecordInterview.svg" className="w-full h-full" alt="" />
+                    <img src="images\RecordInterview.svg" className="w-full  h-48" alt="" />
+                    <div className='text-center font-bold my-1'>RECORD INTERVIEW</div>
                     <div className="mt-3 p-1 w-56 flex mx-auto">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus est mi,
-                        molestie et tellus sit amet, rutrum mollis felis.
+                        Start recording yourself answering the questions appearing on the screen and
+                        end the recording once you are done.
                     </div>
                 </div>
             </div>
